8a7d1c31c7f0f474ad5934bd01e66300b622e3e9
Kévin JAMET <keke.jamet@gmail.com>
2025-02-20 19:51:50 +0100

    feat: added latex support to nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,6 +7,12 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.bo.softtabstop = 4
 
+-- LaTeX configuration
+
+vim.g.vimtex_view_general_viewer = "/home/kj/.local/bin/sumatrapdf.sh"
+vim.g.vimtex_view_general_options =
+    "-reuse-instance -forward-search @tex @line @pdf"
+
 -- Change Theme
 vim.cmd("colorscheme onedark")
 -- vim.cmd("colorscheme onelight")
