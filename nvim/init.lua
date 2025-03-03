4f4153429ef1c80c735fad54ab8f98ee1bec40d3
Kévin JAMET <keke.jamet@gmail.com>
2025-03-03 20:23:59 +0100

    feat: CCC for neovim CSS colors

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,12 +7,29 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.bo.softtabstop = 4
 
+-- Change Theme
+vim.cmd("colorscheme onedark")
+-- vim.cmd("colorscheme onelight")
+
+---------------------------------------------
+
 -- LaTeX configuration
 
 vim.g.vimtex_view_general_viewer = "/home/kj/.local/bin/sumatrapdf.sh"
 vim.g.vimtex_view_general_options =
     "-reuse-instance -forward-search @tex @line @pdf"
 
--- Change Theme
-vim.cmd("colorscheme onedark")
--- vim.cmd("colorscheme onelight")
+-- CSS Colors
+vim.opt.termguicolors = true
+local ccc = require("ccc")
+local mapping = ccc.mapping
+ccc.setup({
+    -- Your preferred settings
+    -- Example: enable highlighter
+    highlighter = {
+        auto_enable = true,
+        lsp = true,
+    },
+})
+
+-- vim.g.vimspector_enable_mappings = "HUMAN"
